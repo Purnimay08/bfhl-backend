b1d3d494585e295e7fe71dd8a8e12c0733aa0622
Purnimay08 <purnimay2005@gmail.com>
2025-02-21 20:33:22 +0530

    Added root route

--- index.js
+++ index.js
@@ -54,6 +54,11 @@ app.get('/bfhl', (req, res) => {
     res.status(200).json({ operation_code: 1 });
 });
 
+app.get('/', (req, res) => {
+    res.send('Backend is running ');
+  });
+  
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
